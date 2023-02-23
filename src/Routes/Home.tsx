f3cfcd1fdb561217692d22011fc2169153260da5
dong1hyun <limd1238@naver.com>
2023-02-23 17:06:12 +0900

    slider에 포스터 넣기 완료

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -43,59 +43,81 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
     display: grid;
-    gap: 10px;
+    gap: 5px;
     grid-template-columns: repeat(6, 1fr);
     position: absolute;
     width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{bgPhoto: string}>`
     background-color: white;
     height: 200px;
-    color: red;
-    font-size: 64px;
+    font-size: 32px;
+    background-image: url(${props => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
 `;
 
 const rowVariants = {
     hidden: {
-        x: window.outerWidth + 10,
+        x: window.outerWidth - 5,
     },
     visible: {
         x: 0,
     },
     exit: {
-        x: -window.outerWidth - 10
+        x: -window.outerWidth + 5
     }
 }
+
+const offset = 6;
 
 function Home() {
     const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
     console.log(data);
     const [index, setIndex] = useState(0);
-    const increaseIndex = () => setIndex((prev) => prev + 1)
+    const [leaving, setLeaving] = useState(false);
+    const increaseIndex = () => {
+        if (leaving) return;
+        setLeaving(true);
+        if (data) {
+            const totalMovies = data.results.length - 1;
+            const maxIndex = Math.floor(totalMovies / offset) - 1;
+            setIndex((prev) => prev === maxIndex ? 0 : prev + 1);
+        }
+
+    };
+    /*처음 렌더링 될 때부터 animate 되는 것 방지 <AnimatePresence initial={false}*/
+    const toggleLeaving = () => setLeaving((prev) => !prev);
     return (
         <Wrapper>
             {isLoading ? (
                 <Loader>Loading...</Loader>
             ) : (
                 <>
-                    <Banner onClick={increaseIndex} bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+                    <Banner
+                        onClick={increaseIndex}
+                        bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+                    >
                         <Title>{data?.results[0].title}</Title>
                         <Overview>{data?.results[0].overview}</Overview>
                     </Banner>
                     <Slider>
-                        <AnimatePresence>
+                        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
                             <Row
                                 variants={rowVariants}
                                 initial="hidden"
                                 animate="visible"
                                 exit="exit"
-                                transition={{type:"tween", duration: 1}}
+                                transition={{ type: "tween", duration: 1 }}
                                 key={index}
                             >
-                                {[1, 2, 3, 4, 5, 6].map((i) => (
-                                    <Box key={i}>{i}</Box>)
-                                )}
+                                {data?.results.slice(1).slice(offset * index, (index + 1) * offset).map((movie) => (
+                                    <Box
+                                        bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                        key={movie.id}
+                                    />
+                                ))}
                             </Row>
                         </AnimatePresence>
                     </Slider>
